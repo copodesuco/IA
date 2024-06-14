657bf0912d3563f67980823e90fe3502842d312c
copodesuco <134527218+copodesuco@users.noreply.github.com>
2024-06-14 08:53:21 -0300

    Add files via upload

--- main (1).js
+++ main (1).js
@@ -6,8 +6,8 @@ const textoResultado = document.querySelector('.texto-resultado');
 
 const perguntas = [
  {
-      enunciado: "Insira o enunciado da Pergunta 1",
-      alternativa: ["Alternativas 1","Alternativas 2" ]
+      enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as duvidas que uma pessoa pode ter, ele tambem gera imagens e audios hiper-realista. Qual o primeiro oensamento?",
+      alternativa: ["Isso é assustador!","Isso é maravilhoso!" ]
  },
  {
     enuciado: "insira o enunciado da Pergunta 2",
